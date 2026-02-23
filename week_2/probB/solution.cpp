295582296e8be15e870faecbc50d644bc8e55b9e
Harrison Voss <hvoss@iastate.edu>
2026-02-23 13:00:42 -0600

    probB wip

--- week_2/probB/solution.cpp
+++ week_2/probB/solution.cpp
@@ -14,10 +14,23 @@ int main() {
   for (auto a : numbers)
     std::cout << a << std::endl;
 
-  // We can calculate the max and min to right and left by looping once from each side
-  // The max of left and min of right is all we need to determine pivot
-  // We can precalc them based on the index of pivot by looping from both sides and comparing max/min
+  // We can calculate the max and min to right and left by looping once from
+  // each side The max of left and min of right is all we need to determine
+  // pivot We can precalc them based on the index of pivot by looping from both
+  // sides and comparing max/min
 
+  std::vector<int> left_max(n);
+  std::vector<int> right_min;
 
+  // wrong
+  left_max.push_back(numbers[0]);
+  for (int i = 1; i < n; i++) {
+    if (left_max[i - 1] < numbers[i]) {
+      left_max[i] = numbers[i];
+    }
+  }
 
+  printf("\n\n");
+  for (auto a : left_max)
+    std::cout << a << std::endl;
 }
