5854c4d07c51cbdf7e7ba55a6fa5e02bac0cb5e8
Harrison Voss <hvoss@iastate.edu>
2026-02-28 01:26:41 -0600

    logic or indexing problem in the min and max loops, but so close

--- week_2/probB/solution.cpp
+++ week_2/probB/solution.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <iostream>
 
 int main() {
   int n;
@@ -11,8 +12,6 @@ int main() {
     numbers.push_back(temp);
   }
 
-  for (auto a : numbers)
-    std::cout << a << std::endl;
 
   // We can calculate the max and min to right and left by looping once from
   // each side The max of left and min of right is all we need to determine
@@ -20,17 +19,52 @@ int main() {
   // sides and comparing max/min
 
   std::vector<int> left_max(n);
-  std::vector<int> right_min;
+  std::vector<int> right_min(n);
 
-  // wrong
-  left_max.push_back(numbers[0]);
+
+  // both of these are not skipping the current pivot in the calc of min and max
+  // so on piv:20 the right_min can be 20, and the max is not keeping the righ number across the indexes
+  left_max[0] = 0;
   for (int i = 1; i < n; i++) {
     if (left_max[i - 1] < numbers[i]) {
-      left_max[i] = numbers[i];
+      left_max[i] = numbers[i-1];
+    } else {
+      left_max[i] = left_max[i-1];
+    }
+    printf("%d\n", i);
+  }
+
+  printf("\n\n");
+  std::cout << left_max.size() << std::endl;
+  for (auto a : left_max)
+    std::cout << a << std::endl;
+
+  right_min[n-1] = numbers[n-1];
+  for (int i = n-2; i > 0; i--) {
+    if (right_min[i+1] > numbers[i]) {
+      right_min[i] = numbers[i];
+    } else {
+      right_min[i] = right_min[i+1];
     }
   }
 
   printf("\n\n");
-  for (auto a : left_max)
+  std::cout << right_min.size() << std::endl;
+  for (auto a : right_min)
+    std::cout << a << std::endl;
+
+  std::vector<int> pivots;
+
+  for (int i = 0; i < n; i++) {
+    printf("piv: %d -- left_max: %d -- right_min: %d\n", numbers[i], left_max[i], right_min[i]);
+    if (left_max[i] <= numbers[i] && right_min[i] > numbers[i]) {
+      printf("if entered\n");
+      pivots.push_back(numbers[i]);
+    }
+  }
+
+  printf("\n\n");
+  std::cout << pivots.size() << std::endl;
+  for (auto a : pivots)
     std::cout << a << std::endl;
 }
